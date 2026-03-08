e3fb7a95e0f3e986bcfa9384a70c013840a3c10e
Geoff McQueen <geoff@geoff.space>
2026-03-08 03:02:29 -0700

    More tests for builder

--- test/src/TestPoolBuilder.cpp
+++ test/src/TestPoolBuilder.cpp
@@ -17,6 +17,49 @@ TEST(PoolBuilder, Name) {
     EXPECT_EQ(EXPECTED_NAME, p.get_name());
 }
 
+TEST(PoolBuilder, TargetParallelism) {
+    pool_builder b;
+    constexpr auto PARALLELISM = 561;
+    b.set_target_parallelism(PARALLELISM);
+    EXPECT_EQ(PARALLELISM, b.get_target_parallelism());
+}
+
+TEST(PoolBuilder, ExtraThreads) {
+    pool_builder b;
+    constexpr auto EXTRA_THREADS = 518;
+    b.set_extra_threads(EXTRA_THREADS);
+    EXPECT_EQ(EXTRA_THREADS, b.get_extra_threads().value());
+}
+
+TEST(PoolBuilder, AllowWorkOffPoolThreads) {
+    pool_builder b;
+    b.set_allow_work_off_pool_threads(false);
+    EXPECT_FALSE(b.get_allow_work_off_pool_threads());
+    b.set_allow_work_off_pool_threads(true);
+    EXPECT_TRUE(b.get_allow_work_off_pool_threads());
+    b.set_allow_work_off_pool_threads(false);
+    EXPECT_FALSE(b.get_allow_work_off_pool_threads());
+}
+
+TEST(PoolBuilder, QueueSize) {
+    pool_builder b;
+    constexpr auto QUEUE_SIZE = 98765;
+    b.set_queue_size(QUEUE_SIZE);
+    EXPECT_EQ(QUEUE_SIZE, b.get_queue_size());
+}
+
+void testFullQueuePolicy(FullQueuePolicy fqp) {
+    pool_builder b;
+    b.set_full_queue_policy(fqp);
+    EXPECT_EQ(fqp, b.get_full_queue_policy());
+}
+
+TEST(PoolBuilder, FullQueuePolicy) {
+    testFullQueuePolicy(FullQueuePolicy::Block);
+    testFullQueuePolicy(FullQueuePolicy::DropOld);
+    testFullQueuePolicy(FullQueuePolicy::DropNew);
+}
+
 TEST(PoolBuilder, ThreadFactory) {
     pool_builder b;
     std::atomic<int> count = 0;
@@ -33,6 +76,7 @@ TEST(PoolBuilder, ThreadFactory) {
     } // Ensure all threads have started and joined.
     EXPECT_EQ(TARGET_THREADS + EXTRA_THREADS, count);
 }
+
 
 TEST(PoolBuilder, BuildTwice) {
     pool_builder b;
